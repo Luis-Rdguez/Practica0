c41546686bc6fc28fba849ffbf9a1662081be5cb
IsmaBona <ismael.bonachia@opendeusto.es>
2022-09-25 20:32:03 +0200

    guardar csv

--- Practica0/src/Clases/DatoTabular.java
+++ Practica0/src/Clases/DatoTabular.java
@@ -75,7 +75,7 @@ public abstract class DatoTabular {
 
 		    }}
 		}
-	
+	//guardar csv
 	public static void guardarCSV() {
 
 			try {
--- Practica0/src/Clases/TablaDatos.java
+++ Practica0/src/Clases/TablaDatos.java
@@ -6,7 +6,11 @@ import javax.swing.JTable;
 
 
 public class TablaDatos extends JTable {
-	
+	public TablaDatos() {
+		setTitle("grandslams");
+		JTable TablaDatos = new JTable();
+		add(new JScrollPanel(TablaDatos), BorderLayout.CENTER);
+	}
 	
 	
 	
