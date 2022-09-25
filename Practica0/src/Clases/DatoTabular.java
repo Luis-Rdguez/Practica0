571ec28426aec621be87c1dbac713435600edb26
IsmaBona <ismael.bonachia@opendeusto.es>
2022-09-25 19:51:59 +0200

    cargar csv

--- Practica0/src/Clases/DatoTabular.java
+++ Practica0/src/Clases/DatoTabular.java
@@ -1,5 +1,9 @@
 package Clases;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public abstract class DatoTabular {
@@ -48,10 +52,35 @@ public abstract class DatoTabular {
 	}
 	
 	public static void cargaCSV() {
-		
-	}
+		String csvFile = "/Practica0/src/Australian Open Championships, Champion vs Runner-up, Men's Singles,1969 - 2021.csv";
+		BufferedReader br = null;
+		String line = "";
+		//Se define separador ","
+		String cvsSplitBy = ",";
+		try {
+		    br = new BufferedReader(new FileReader(csvFile));
+		    while ((line = br.readLine()) != null) {                
+		        String[] datos = line.split(cvsSplitBy);
+		        //Imprime datos.
+		       System.out.println(datos[0] + ", " + datos[1] + ", " + datos[2] + ", " + datos[3] + ", " + datos[4] + ", " + datos[5]);
+		    }
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} finally {
+		    if (br != null) {
+		        try {
+		            br.close();
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+
+		    }}
+		}
 	
 	public static void guardarCSV() {
+		
 		
 	}
 }
